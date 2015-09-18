275b1f8171e4ebe5bf0e390372a072a712a03553
Andrew Kishino <AndrewKishino@gmail.com>
2015-09-18 11:36:48 -0700

    Updating to match npm version

--- bart.js
+++ bart.js
@@ -1,125 +1,129 @@
-var bart = require('bart').createClient();
+var bart = require('working-bart').createClient();
 var _ = require('underscore');
 
-var handler = function(data, ferd) {
-
-  var stations = {
-    '12th' : '12th St. Oakland City Center',
-    '16th' : '16th St. Mission',
-    '19th' : '19th St. Oakland',
-    '24th' : '24th St. Mission',
-    'ashb' : 'Ashby',
-    'balb' : 'Balboa Park',
-    'bayf' : 'Bay Fair',
-    'cast' : 'Castro Valley',
-    'civc' : 'Civic Center',
-    'cols' : 'Coliseum/Oakland Airport',
-    'colm' : 'Colma',
-    'conc' : 'Concord',
-    'daly' : 'Daly City',
-    'dbrk' : 'Downtown Berkeley',
-    'dubl' : 'Dublin/Pleasanton',
-    'deln' : 'El Cerrito del Norte',
-    'plza' : 'El Cerrito Plaza',
-    'embr' : 'Embarcadero',
-    'frmt' : 'Fremont',
-    'ftvl' : 'Fruitvale',
-    'glen' : 'Glen Park',
-    'hayw' : 'Hayward',
-    'lafy' : 'Lafayette',
-    'lake' : 'Lake Merritt',
-    'mcar' : 'MacArthur',
-    'mlbr' : 'Millbrae',
-    'mont' : 'Montgomery St.',
-    'nbrk' : 'North Berkeley',
-    'ncon' : 'North Concord/Martinez',
-    'orin' : 'Orinda',
-    'pitt' : 'Pittsburg/Bay Point',
-    'phil' : 'Pleasant Hill',
-    'powl' : 'Powell St.',
-    'rich' : 'Richmond',
-    'rock' : 'Rockridge',
-    'sbrn' : 'San Bruno',
-    'sfia' : 'San Francisco Intl Airport',
-    'sanl' : 'San Leandro',
-    'shay' : 'South Hayward',
-    'ssan' : 'South San Francisco',
-    'ucty' : 'Union City',
-    'wcrk' : 'Walnut Creek',
-    'wdub' : 'West Dublin',
-    'woak' : 'West Oakland'
-  };
-
-  if(data.ferd.text === 'map' || data.ferd.text === '') {
-
-    var bartMap = [{
-          "fallback": 'BART abbreviations',
-          "text": "Usage: `ferd bart` *`powl`* with one of these station codes: ",
-          "color": '#ffffff',
-          "image_url": "http://i.imgur.com/WM9ROJJ.png",
-          "mrkdwn_in": ["text"]
-        }];
-    ferd.sendMessage({
-      channel: data.channel,
-      as_user: true,
-      attachments: JSON.stringify(bartMap),
-      mrkdwn: true
-    });
-  } else {
-    if(stations[data.ferd.text]) {
-      var leadingText = 'From *' + stations[data.ferd.text] + '* station:\n\n';
-      bart.on(data.ferd.text, function (estimates) {
-
-        // collapse trains for the same destination
-        var collapse = function (trains) {
-          return _.chain(trains)
-            .groupBy(function (train) {
-              return train.destination;
-            })
-            .sortBy(function (train) {
-              return train.destination;
-            });
-        };
-        
-        var lines = collapse(estimates);
-
-        var attachments = _(lines).reduce(function (memo, line) {
-              var dest = line[0].destination,
-                  times = _(line).reduce(function (memo, train) {
-                    return memo.concat('`'+train.minutes+'`');
-                    }, []).join(', ');
-              var attachment = {
-                    "fallback": dest + ' in ' + times + ' minutes.',
-                    "text": '*' + dest + '* in ' + times + ' minutes.',
-                    "color": line[0].hexcolor,
-                    "mrkdwn_in": ["text"]
-                  };
-              return memo.concat(attachment);
-            }, []);
-          
-        ferd.sendMessage({
-          channel: data.channel,
-          as_user: true,
-          text: leadingText,
-          mrkdwn: true,
-          attachments: JSON.stringify(attachments)
-        });
-
-        // this line stops recurring messages
-        this.removeAllListeners();
-
-      });
-    } else {
-      ferd.sendMessage({
-        channel: data.channel,
-        as_user: true,
-        text: 'There is no Bart station with that station code. Type `ferd bart help` to list Bart station codes.',
-        mrkdwn: true
-      });
-    }
-  }
+var stations = {
+  '12th' : '12th St. Oakland City Center',
+  '16th' : '16th St. Mission',
+  '19th' : '19th St. Oakland',
+  '24th' : '24th St. Mission',
+  'ashb' : 'Ashby',
+  'balb' : 'Balboa Park',
+  'bayf' : 'Bay Fair',
+  'cast' : 'Castro Valley',
+  'civc' : 'Civic Center',
+  'cols' : 'Coliseum/Oakland Airport',
+  'colm' : 'Colma',
+  'conc' : 'Concord',
+  'daly' : 'Daly City',
+  'dbrk' : 'Downtown Berkeley',
+  'dubl' : 'Dublin/Pleasanton',
+  'deln' : 'El Cerrito del Norte',
+  'plza' : 'El Cerrito Plaza',
+  'embr' : 'Embarcadero',
+  'frmt' : 'Fremont',
+  'ftvl' : 'Fruitvale',
+  'glen' : 'Glen Park',
+  'hayw' : 'Hayward',
+  'lafy' : 'Lafayette',
+  'lake' : 'Lake Merritt',
+  'mcar' : 'MacArthur',
+  'mlbr' : 'Millbrae',
+  'mont' : 'Montgomery St.',
+  'nbrk' : 'North Berkeley',
+  'ncon' : 'North Concord/Martinez',
+  'orin' : 'Orinda',
+  'pitt' : 'Pittsburg/Bay Point',
+  'phil' : 'Pleasant Hill',
+  'powl' : 'Powell St.',
+  'rich' : 'Richmond',
+  'rock' : 'Rockridge',
+  'sbrn' : 'San Bruno',
+  'sfia' : 'San Francisco Intl Airport',
+  'sanl' : 'San Leandro',
+  'shay' : 'South Hayward',
+  'ssan' : 'South San Francisco',
+  'ucty' : 'Union City',
+  'wcrk' : 'Walnut Creek',
+  'wdub' : 'West Dublin',
+  'woak' : 'West Oakland'
 };
 
-module.exports = function(data, ferd) {
-  handler(data, ferd);
+module.exports = function(ferd) {
+
+  ferd.listen(/bart\s(12th|16th|19th|24th|ashb|balb|bayf|cast|civc|cols|colm|conc|daly|dbrk|dubl|deln|plza|embr|frmt|ftvl|glen|hayw|lafy|lake|mcar|mlbr|mont|nbrk|ncon|orin|pitt|phil|powl|rich|rock|sbrn|sfia|sanl|shay|ssan|ucty|wcrk|wdub|woak|help|map)/i, function(response) {
+    var option = response.match[1];
+
+    var from = response.incomingMessage.user;
+    var to = response.slack.self.id;
+
+    var getBart = function(param) {
+      if(from !== to) {
+        if(param === 'map' || param === 'help') {
+
+          var bartMap = [{
+                'fallback': 'BART abbreviations',
+                'text': 'Usage: `ferd bart` *`powl`* with one of these station codes: ',
+                'color': '#ffffff',
+                'image_url': 'http://i.imgur.com/WM9ROJJ.png',
+                'mrkdwn_in': ['text']
+              }];
+
+          response.postMessage({
+            as_user: true,
+            attachments: bartMap,
+            mrkdwn: true
+          });
+
+        } else {
+          if(stations[param]) {
+            var leadingText = 'From *' + stations[param] + '* station:\n\n';
+            bart.on(param, function (estimates) {
+
+              // collapse trains for the same destination
+              var collapse = function (trains) {
+                return _.chain(trains)
+                  .groupBy(function (train) {
+                    return train.destination;
+                  })
+                  .sortBy(function (train) {
+                    return train.destination;
+                  });
+              };
+              
+              var lines = collapse(estimates);
+
+              var attachments = _(lines).reduce(function (memo, line) {
+                    var dest = line[0].destination,
+                        times = _(line).reduce(function (memo, train) {
+                          return memo.concat('`'+train.minutes+'`');
+                          }, []).join(', ');
+                    var attachment = {
+                          'fallback': dest + ' in ' + times + ' minutes.',
+                          'text': '*' + dest + '* in ' + times + ' minutes.',
+                          'color': line[0].hexcolor,
+                          'mrkdwn_in': ['text']
+                        };
+                    return memo.concat(attachment);
+                  }, []);
+             
+              response.postMessage({
+                as_user: true,
+                text: leadingText,
+                mrkdwn: true,
+                attachments: attachments
+              });
+
+            });
+          } else {
+            response.postMessage({
+              as_user: true,
+              text: 'There is no Bart station with that station code. Type `ferd bart help` to list Bart station codes.',
+              mrkdwn: true
+            });
+          }
+        }
+      }
+    };
+    getBart(option);
+  });
 };
